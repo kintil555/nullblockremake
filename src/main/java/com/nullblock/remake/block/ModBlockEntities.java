cc0b7e78e49a15a23a9f4f130ec7144f4f9ddcb4
kintil555 <dzikriganteng52637@gmail.com>
2026-07-25 20:09:59 +0700

    fix: ganti BlockEntityType.Builder (Forge-only) dengan FabricBlockEntityTypeBuilder

--- src/main/java/com/nullblock/remake/block/ModBlockEntities.java
+++ src/main/java/com/nullblock/remake/block/ModBlockEntities.java
@@ -2,6 +2,7 @@ package com.nullblock.remake.block;
 
 import com.nullblock.remake.NullBlockRemakeMod;
 import com.nullblock.remake.block.entity.NullBlockEntity;
+import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
@@ -12,7 +13,7 @@ public class ModBlockEntities {
     public static final BlockEntityType<NullBlockEntity> NULL_BLOCK_ENTITY = Registry.register(
             BuiltInRegistries.BLOCK_ENTITY_TYPE,
             ResourceLocation.fromNamespaceAndPath(NullBlockRemakeMod.MODID, "null_block_entity"),
-            BlockEntityType.Builder.of(NullBlockEntity::new, ModBlocks.NULL_BLOCK).build(null)
+            FabricBlockEntityTypeBuilder.create(NullBlockEntity::new, ModBlocks.NULL_BLOCK).build()
     );
 
     public static void init() {
