561bdb0aee73236d9c9f82c28f9c627440d0d794
kintil555 <dzikriganteng52637@gmail.com>
2026-07-26 09:59:55 +0700

    fix: correct BlockRenderLayerMap package for MC 1.21.4 (blockrenderlayer.v1, not client.rendering.v1)

--- src/main/java/com/nullblock/remake/client/NullBlockRemakeModClient.java
+++ src/main/java/com/nullblock/remake/client/NullBlockRemakeModClient.java
@@ -4,7 +4,7 @@ import com.nullblock.remake.block.ModBlockEntities;
 import com.nullblock.remake.block.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererRegistry;
-import net.fabricmc.fabric.api.client.rendering.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.renderer.RenderType;
 
