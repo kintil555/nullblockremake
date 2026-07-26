a4af08ed2f2141e61e0809cb6c65b5e9f50019a0
kintil555 <dzikriganteng52637@gmail.com>
2026-07-26 14:53:33 +0700

    Fix: gunakan Mojmap (FlowingFluid, isSolidFace) sesuai loom.officialMojangMappings(), sebelumnya salah pakai package Yarn

--- src/main/java/com/nullblock/remake/mixin/FlowableFluidMixin.java
+++ src/main/java/com/nullblock/remake/mixin/FlowableFluidMixin.java
@@ -1,23 +1,22 @@
 package com.nullblock.remake.mixin;
 
 import com.nullblock.remake.block.NullBlock;
-import net.minecraft.block.BlockState;
-import net.minecraft.fluid.FlowableFluid;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.world.BlockView;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.material.FlowingFluid;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(FlowableFluid.class)
+@Mixin(FlowingFluid.class)
 public abstract class FlowableFluidMixin {
 
-    @Inject(method = "isFlowBlocked", at = @At("HEAD"), cancellable = true)
-    private void nullblock_remake$treatAsSolidFace(BlockView world, BlockPos pos, Direction direction,
+    @Inject(method = "isSolidFace", at = @At("HEAD"), cancellable = true)
+    private void nullblock_remake$treatAsSolidFace(BlockGetter level, BlockPos neighborPos, Direction side,
                                                      CallbackInfoReturnable<Boolean> cir) {
-        if (world.getBlockState(pos).getBlock() instanceof NullBlock) {
+        if (level.getBlockState(neighborPos).getBlock() instanceof NullBlock) {
             cir.setReturnValue(true);
         }
     }
